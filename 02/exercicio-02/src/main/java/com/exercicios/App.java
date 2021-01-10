e4450f1fc495e5fd1a47414a10a06e68091296f6
Carolina <carolina.kaji@gmail.com>
2021-01-10 20:56:30 -0300

    Circle exercise

---
+++ 02/exercicio-02/src/main/java/com/exercicios/App.java
@@ -0,0 +1,31 @@
+package com.exercicios;
+
+/**
+ * Escreva uma classe Circulo para representar círculos. Esta classe deverá
+ * conter os seguintes atributos e métodos:
+ * 
+ * a) Uma variável privada do tipo double chamada raio que guardará o valor do
+ * raio atual do circulo.
+ * 
+ * b) Métodos get() e set() públicos que permitem definir e acessar o valor da
+ * variável raio.
+ * 
+ * c) Um construtor que recebe o valor do raio como argumento.
+ * 
+ * d) Um método getArea() que calcula e retorna a área do círculo.
+ * 
+ * e) Um método getCircunferencia() que calcula e retorna a circunferência do
+ * círculo.
+ * 
+ * f) Um método aumentarRaio() que recebe um valor double e o usa como
+ * percentual para aumentar o raio do círculo.
+ * 
+ * Crie uma classe contendo um método main() para testar a funcionalidade da
+ * classe Circulo. Eis um exemplo de entrada e saída:
+ *
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
